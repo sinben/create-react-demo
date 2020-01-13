5e22f12473e9ab9fa46ef911ed24557d860ab6fd
Sindre Benonisen <sbeno@equinor.com>
2020-01-13 08:40:37 +0100

    Moved provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Provider } from "react-redux";
 import { Route, Switch } from "react-router";
 import List from "./components/imp-list/imp-list";
 import Details from "./components/imp-details/imp-details";
 import {initStore} from './redux/store';
 
-const store = initStore();
+
 
 const App: React.FC = () => {
   return (
       <>
-      <Provider store={store}>
             <Switch>
               <Route exact path="/" component={List} />
               <Route path="/imp/:id" component={Details} />
             </Switch>
-      </Provider>
       </>
   );
 }
